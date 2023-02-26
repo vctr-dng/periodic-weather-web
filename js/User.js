8dad19e10bd8e41622f13ba0910ce9684120beb6
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2023-02-26 15:42:53 +0100

    fetch coordinates based on city provided by randomUser API; new users added to queue

--- js/User.js
+++ js/User.js
@@ -1,11 +1,27 @@
 export { User }
 
+var userAPI_URL = "https://randomuser.me/api/?nat=fr&inc=name,location&noinfo"
+
 class User {
 
     constructor(first, last, coordinates) {
         this.first = first
         this.last = last
         this.coordinates = coordinates // [longitude, latitude]
+    }
+
+    static async createUser() {
+        var randomUser = await User.requestNewUser();
+
+        console.log(randomUser)
+    
+        let correctCoordinates = await User.getCoordinatesFromCity(randomUser.location.city)
+        
+        console.log(correctCoordinates)
+
+        let user = new User(randomUser.name.first, randomUser.name.last, correctCoordinates)
+
+        return user;
     }
 
     static createUserFromRandomUser(dataAPI) {
@@ -21,5 +37,35 @@ class User {
         let coordinates = dataAPI['location']['coordinates']
 
         return new User(first, last, coordinates)
+    }
+    
+    static async requestNewUser() {
+        const response = await fetch(userAPI_URL)
+    
+        /*
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                return data.results['0']
+            })
+            .then((individualData) => {
+                return individualData
+            })
+        */
+    
+        const data = await response.json()
+        const individualData = await data.results['0']
+        
+        return individualData
+    }
+    
+    static async getCoordinatesFromCity(city) {
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`)
+        const data = await response.json()
+        const geolocationData = await data.results['0']
+        const coordinates = [geolocationData.longitude, geolocationData.latitude]
+
+        return coordinates
     }
 }
--- js/front.js
+++ js/front.js
@@ -2,45 +2,19 @@ import {UserQueue} from './UserQueue.js'
 import {User} from './User.js'
 
 var maxUser = 10
-var userAPI_URL = "https://randomuser.me/api/?nat=fr&inc=name,location&noinfo"
+var msInterval = 1000
 
 function init() {
 
     var userQueue = createUserQueue(maxUser)
 
-    let usr = createUser()
-
-}
-
-async function createUser() {
-    var userFromAPI = await requestNewUser();
-    console.log("here is the individual")
-    console.log(userFromAPI)
-
-    let user = User.createUserFromRandomUser(userFromAPI)
-
-    console.log(user)
-}
-
-async function requestNewUser() {
-    const response = await fetch(userAPI_URL)
-
-    /*
-        .then((response) => {
-            return response.json()
-        })
-        .then((data) => {
-            return data.results['0']
-        })
-        .then((individualData) => {
-            return individualData
-        })
-    */
-
-    const data = await response.json()
-    const individualData = data.results['0']
-    
-    return individualData
+    setInterval(async() => {
+        let usr = await User.createUser()
+        console.log(usr)
+        userQueue.add(usr)
+        console.log(userQueue)
+    },
+    msInterval)
 }
 
 function addUser() {
